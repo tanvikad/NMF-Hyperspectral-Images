926a44b1a390b6f5bdb431e86421129894374a8d
Kenneth-Mitchell <76679561+Kenneth-Mitchell@users.noreply.github.com>
2023-12-03 11:29:07 -0800

    added rmse and rmnf

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ from scipy.io import loadmat
 from metrics import find_similarity
 from models.nmf_hs import nmf_hs
 
+from models.rnmf import robust_nmf
 
 class NMF_Models:
-    def __init__(self, num_endmembers=6, reload=False):
+    def __init__(self, num_endmembers=6, reload=True):
         self.hs_image = sio.loadmat("data/Urban_R162.mat")
         self.X = np.array(self.hs_image["Y"]).astype(np.float64)
         self.num_endmembers = num_endmembers
         self.set_w_gt()
         self.W_nmf_hs = None 
+        self.W_rnmf = None
         if(not reload): self.load_matrices()
-
 
     def save_matrices(self):
         mat_dic = {}
         if(self.W_nmf_hs is not None):
             self.W_nmf_hs = self.W_nmf_hs.astype(np.float64) 
             mat_dic["W_nmf_hs"] = self.W_nmf_hs
+
+        if(self.W_rnmf is not None):
+            self.W_rnmf = self.W_rnmf.astype(np.float64) 
+            mat_dic["W_rnmf"] = self.W_rnmf
+
         savemat("generated_data/data.mat", mat_dic)
 
     def load_matrices(self):
@@ -56,6 +62,7 @@ class NMF_Models:
         
         pathname = "img/" + title + ".png"
         plt.savefig(pathname)
+        plt.clf()
 
     def run_nmf_hs(self):
         if(self.W_nmf_hs is None):
@@ -63,6 +70,22 @@ class NMF_Models:
             self.save_matrices()
         err, best_ordering = find_similarity(self.W_nmf_hs, self.W_gt)
         self.plot_endmembers(self.W_nmf_hs, title="NMF_HS", ordering=best_ordering)
+        print("The error is", err)
+        
+
+    def run_rnmf(self):
+        if(self.W_rnmf is None):
+            self.W_rnmf, H, outlier, obj = robust_nmf(self.X,
+                                            rank=self.num_endmembers,
+                                            beta=1.5,
+                                            init='NMF',
+                                            reg_val=1,
+                                            sum_to_one=0,
+                                            tol=1e-7,
+                                            max_iter=50)
+            self.save_matrices()
+        err, best_ordering = find_similarity(self.W_rnmf, self.W_gt)
+        self.plot_endmembers(self.W_rnmf, title="RNMF", ordering = best_ordering)
         print("The error is", err)
 
     def plot_gt(self):
@@ -72,3 +95,4 @@ class NMF_Models:
 models = NMF_Models()
 models.plot_gt()
 models.run_nmf_hs()
+models.run_rnmf()
--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 from scipy import spatial
 from itertools import permutations
 import numpy as np 
+from sklearn.metrics import mean_squared_error
 
 
 
@@ -18,6 +19,7 @@ def find_similarity(W, W_gt, num_components=6):
         for i in range(len(perm)):
             if(dp[perm[i]][i] == -1):
                 dp[perm[i]][i] = cosine_similarity(W[:,perm[i]], W_gt[:,i])
+                # dp[perm[i]][i] = min_RMSE(W[:,perm[i]], W_gt[:,i]) 
                 
             current_error += dp[perm[i]][i]
             
@@ -30,3 +32,8 @@ def find_similarity(W, W_gt, num_components=6):
 
 def cosine_similarity(vec1, vec2):
     return spatial.distance.cosine(vec1, vec2)
+
+def min_RMSE(vec1,vec2):
+    norm_vec1 = vec1 / np.linalg.norm(vec1)
+    norm_vec2 = vec2 / np.linalg.norm(vec2)
+    return mean_squared_error(norm_vec1.transpose(), norm_vec2.transpose(), squared=False)
