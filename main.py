9a7923697beb869d07552ccec854a63cf1d152c6
tanvikad <tanvika.dasari@gmail.com>
2023-12-02 20:45:35 -0800

    added loaded data

--- main.py
+++ main.py
@@ -3,17 +3,35 @@ import numpy as np
 from sklearn.decomposition import NMF 
 from matplotlib import pyplot as plt
 
+from scipy.io import savemat
+from scipy.io import loadmat
+
 from metrics import find_similarity
 from models.nmf_hs import nmf_hs
 
 
 class NMF_Models:
-    def __init__(self, num_endmembers=6):
+    def __init__(self, num_endmembers=6, reload=False):
         self.hs_image = sio.loadmat("data/Urban_R162.mat")
         self.X = np.array(self.hs_image["Y"]).astype(np.float64)
         self.num_endmembers = num_endmembers
         self.set_w_gt()
-        
+        self.W_nmf_hs = None 
+        if(not reload): self.load_matrices()
+
+
+    def save_matrices(self):
+        mat_dic = {}
+        if(self.W_nmf_hs is not None):
+            self.W_nmf_hs = self.W_nmf_hs.astype(np.float64) 
+            mat_dic["W_nmf_hs"] = self.W_nmf_hs
+        savemat("generated_data/data.mat", mat_dic)
+
+    def load_matrices(self):
+        mat_dic = loadmat('generated_data/data.mat')
+        if("W_nmf_hs" in mat_dic):
+            self.W_nmf_hs = mat_dic["W_nmf_hs"]
+
     def set_w_gt(self, endmembers = 6):
         hs_gt_image = None
         if(endmembers == 6):
@@ -40,9 +58,11 @@ class NMF_Models:
         plt.savefig(pathname)
 
     def run_nmf_hs(self):
-        W, H, error = nmf_hs(self.X, 1000.0, 1000, self.num_endmembers)
-        err, best_ordering = find_similarity(W, self.W_gt)
-        self.plot_endmembers(W, title="NMF_HS", ordering=best_ordering)
+        if(self.W_nmf_hs is None):
+            self.W_nmf_hs, H, error = nmf_hs(self.X, 1000.0, 1000, self.num_endmembers)
+            self.save_matrices()
+        err, best_ordering = find_similarity(self.W_nmf_hs, self.W_gt)
+        self.plot_endmembers(self.W_nmf_hs, title="NMF_HS", ordering=best_ordering)
         print("The error is", err)
 
     def plot_gt(self):
